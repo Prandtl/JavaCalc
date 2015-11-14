92494c392118e84c7bcbc98ef2b4cbc5d397d580
Prandtl <prandtl211@gmail.com>
2015-11-14 16:28:45 +0500

    homework - done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Scanner scanner = new Scanner(System.in);
+        System.out.println("Write operation name.");
+        String userInput = scanner.nextLine();
+        System.out.println(userInput);
+
+        System.out.print("Input first number: ");
+        double firstNumber = scanner.nextDouble();
+        System.out.print("Input second number: ");
+        double secondNumber = scanner.nextDouble();
+
+
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+
+        double result = 0;
+
+        switch (userInput){
+            case "add":
+                result = firstNumber + secondNumber;
+                break;
+            case "subtract":
+                result = firstNumber - secondNumber;
+                break;
+            case "multiply":
+                result = firstNumber * secondNumber;
+                break;
+            case "divide":
+                result = firstNumber / secondNumber;
+                break;
+        }
+
+        System.out.println(result);
     }
 }
